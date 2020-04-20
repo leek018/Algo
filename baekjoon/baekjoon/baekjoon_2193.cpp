984c0514acf38b6629a15b008aa70b39ed60bf95
leek <leek018@naver.com>
2020-04-20 21:12:10 +0900

    이항계수는 뭐다? long long 이다

--- baekjoon/baekjoon/baekjoon_2193.cpp
+++ baekjoon/baekjoon/baekjoon_2193.cpp
@@ -2,39 +2,29 @@
 using namespace std;
 
 int N;
-int memo[46][46];
-int combi(int n, int r)
-{
-	
+long long memo[91][91];
+long long combi(int n, int r)
+{	
 	if (n == r || r == 0)
-		return memo[n][r] = 1;	
-	int val = memo[n][r];
+		return memo[n][r] = 1LL;	
+	long long val = memo[n][r];
 	if (val >= 0)
 		return val;
 	return memo[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
-}
-int solve(int one, int N)
-{
-	int n = N - 2 * one+1;
-	int r = one-1;
-	if (n == 0)
-		return 1;	
-	return combi(n+r , r);
 }
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> N;
-	for (int i = 0; i < 45; i++)
-		fill_n(memo[i], 45, -1);
-	int sum = 0;
+	for (int i = 0; i <= 90; i++)
+		fill_n(memo[i], 91, -1);
+	long long  sum = 0;
 	int limit = N / 2;
 	if (N % 2 == 1)
 		limit++;
 	for (int i = 1; i <= limit; i++)
-		sum += solve(i, N);
+		sum += combi(N - i, i-1);
 	cout << sum;
 	return 0;
-
 }

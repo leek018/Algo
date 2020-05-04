740d2e2ab33a5c4d0b252f2edd6364920e485167
경석 이 <leek018@naver.com>
2020-05-04 23:49:39 +0900

    졸려

---
+++ baekjoon/baekjoon/baekjoon_2798.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int arr[100];
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N, M; cin >> N >> M;
+	for (int i = 0; i < N; i++)	
+		cin >> arr[i];
+	int answer = M;
+	for (int i = 0; i < N - 2; i++)
+	{
+		for (int j = i + 1; j < N - 1; j++)
+		{
+			for (int k = j + 1; k < N; k++)
+			{
+				int sum = arr[i] + arr[j] + arr[k];
+				if (M - sum >= 0 && M - sum < M-answer)
+					answer = sum;
+			}
+		}
+	}
+	cout << answer;
+	return 0;
+}
